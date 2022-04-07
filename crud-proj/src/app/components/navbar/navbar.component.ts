165bc78d25b2152dd44f29b976791515dd62f341
YakovTamam <43913396+YakovTamam@users.noreply.github.com>
2022-04-07 04:18:34 +0300

    auto-complete

--- crud-proj/src/app/components/navbar/navbar.component.ts
+++ crud-proj/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { observable, map, startWith, Observable, NEVER } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
+import { Employee } from 'src/app/employee';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +10,40 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private dataSercive: DataService) {}
 
-  ngOnInit(): void {}
+  employeesNames: string[] = [];
+  myControl = new FormControl();
+  filteredOption: Observable<string[]> = NEVER;
+  employees: any;
+
+  ngOnInit(): void {
+    this.getEmployees();
+    this.filteredOption = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string) {
+    const filterValue = value.toLowerCase();
+    return this.employeesNames.filter((employee) =>
+      employee.toLowerCase().includes(filterValue)
+    );
+  }
+
+  getEmployees() {
+    this.dataSercive.getData().subscribe((res) => {
+      this.employees = res;
+      this.employees = this.employees.employee;
+      this.mapData();
+      console.log(this.employeesNames);
+    });
+  }
+
+  mapData() {
+    for (let index = 0; index < this.employees.length; index++) {
+      this.employeesNames[index] = this.employees[index].name;
+    }
+  }
 }
